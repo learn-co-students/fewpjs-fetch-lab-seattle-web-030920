e74a2d38429e92b476ef82bae9846dd773be91f2
Bakugou808 <janu.m.sung@gmail.com>
2020-04-21 20:52:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books", {mode: 'no-cors'})
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
